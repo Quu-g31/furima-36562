80c965386f968ca445b8d854cc473e5c8ab23f43
Quu-g31 <lst-ofzf@docomo.ne.jp>
2021-08-30 16:33:04 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
   validates :nickname, presence: true
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
-  validates :password, presence: true,
-            format: { with: VALID_PASSWORD_REGEX
-            #  message: "は半角6~12文字英大文字・小文字・数字それぞれ１文字以上含む必要があります"
-          }
+  validates :password, format: { with: VALID_PASSWORD_REGEX}
   with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/ } do
     validates :last_name
     validates :first_name
-    with_options format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ } do
-      validates :last_name_kana
-      validates :first_name_kana
-    end
+  end
+  with_options presence: true, format: {with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
   end
   validates :birth_day, presence: true
 end
