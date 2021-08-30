b60191c442f2834603c97ddc06aad49c1b6f7b0b
Quu-g31 <lst-ofzf@docomo.ne.jp>
2021-08-30 11:02:16 +0900

    バリデーションの設置

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
+  validates :password, presence: true,
+            format: { with: VALID_PASSWORD_REGEX
+            #  message: "は半角6~12文字英大文字・小文字・数字それぞれ１文字以上含む必要があります"
+          }
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/ } do
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  validates :birth_day, presence: true
+end
