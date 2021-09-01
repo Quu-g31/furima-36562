91f3dcfd8cf0737319aadace34acff994a98dc82
Quu-g31 <lst-ofzf@docomo.ne.jp>
2021-09-01 17:21:25 +0900

    コントローラー各種定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:image, :name, :describe, :category_id, :item_status_id, :delivery_fee_id, :prefecture_id, :send_by_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
