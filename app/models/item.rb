a2df19eafcaa16d2101913950e5dc89d8412812f
Quu-g31 <lst-ofzf@docomo.ne.jp>
2021-09-02 15:31:02 +0900

    バリデーションの設置

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :send_by_day
+
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :describe
+    validates :price, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  end
+  with_options numericality: { other_than: 1, message: "can't be blank"} do
+    validates :category_id
+    validates :item_status_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :send_by_day_id
+  end
 end
