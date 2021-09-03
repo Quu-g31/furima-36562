a747117fa64215359ce24c3b16c3a62c9ffc2377
Quu-g31 <lst-ofzf@docomo.ne.jp>
2021-09-03 23:49:19 +0900

    アソシエーションの定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :item
+
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
 
   validates :nickname, presence: true
