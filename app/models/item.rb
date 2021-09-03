100a6499d01397591d0b1909b55a67bd789e9148
Quu-g31 <lst-ofzf@docomo.ne.jp>
2021-09-03 12:23:11 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :describe
-    validates :price, format: { with: /\A[0-9]+\z/}, numericality: { only_integer: true, greater_than: 300, less_than: 10000000 }
+    validates :price, format: { with: /\A[0-9]+\z/}, numericality: { only_integer: true, greater_than: 299, less_than: 10000000 }
   end
   with_options numericality: { other_than: 1, message: "can't be blank"} do
     validates :category_id
