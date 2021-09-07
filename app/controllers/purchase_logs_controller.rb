58ef285ebeda4c383f3be54e039f3ec11e51e524
Quu-g31 <lst-ofzf@docomo.ne.jp>
2021-09-08 03:05:07 +0900

    purchase_logコントローラーの修正と追加

--- app/controllers/purchase_logs_controller.rb
+++ app/controllers/purchase_logs_controller.rb
@@ -1,15 +1,22 @@
 class PurchaseLogsController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
+    if current_user.id != @item.user_id && @item.purchase_log != nil
+      to_root
+    elsif current_user.id == @item.user_id
+      to_root
+    end
     @purchase_log_address = PurchaseLogAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_log_address = PurchaseLogAddress.new(purchase_log_params)
     if @purchase_log_address.valid?
+      pay_item
       @purchase_log_address.save
-      redirect_to root_path
+      to_root
     else
       render :index
     end
@@ -17,13 +24,25 @@ class PurchaseLogsController < ApplicationController
 
   private
 
-  def purchase_log_params
-    params.require(:purchase_log_address).permit(:postal_code, :prefecture_id, :municipality, :address_code, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
-  # def item_params
-  #   params.require(:item).permit(:image, :name, :describe, :category_id, :item_status_id, :delivery_fee_id, :prefecture_id,
-  #     :send_by_day_id, :price).merge(user_id: current_user.id)
-  # end
+  def to_root
+    redirect_to root_path
+  end
+
+  def purchase_log_params
+    params.require(:purchase_log_address).permit(:postal_code, :prefecture_id, :municipality, :address_code, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_log_params[:token],
+      currency: 'jpy'
+    )
+  end
 
 end
