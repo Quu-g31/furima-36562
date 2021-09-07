116be86b0609399aff9dcc95d0aea9ba4f173c1e
Quu-g31 <lst-ofzf@docomo.ne.jp>
2021-09-08 03:02:09 +0900

    tokenをparamsに含ませる設定

--- app/models/purchase_log_address.rb
+++ app/models/purchase_log_address.rb
@@ -1,6 +1,6 @@
 class PurchaseLogAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipality, :address_code, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :municipality, :address_code, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :user_id
@@ -9,6 +9,7 @@ class PurchaseLogAddress
     validates :municipality
     validates :address_code
     validates :phone_number, format: { with: /\A[0-9]+\z/ }, numericality: {only_integer: true, message: "is invalid. Not include hyphen(-)"}
+    validates :token
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"} 
 
